cb8cfcb6b6f354841884b8bcb0ade1a304504759
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-02-04 13:25:44 +0100

    array db php

---
+++ db.php
@@ -0,0 +1,92 @@
+<?php
+$cars = [
+    [
+        'id' => '1',    
+        'marca' => 'toyota',    
+        'modello' => 'RAV 4',    
+        'motorizzazione' => '2.0 Tdi 115CV',    
+        'anno-immatricolazione' => '06/2002',    
+        'km-percorsi' => '287.000',    
+        'colore' => 'grigio metallizato',    
+        'prezzo' => '2000',    
+        'allestimento' => 'base',    
+        'descrizione-venditore' => "ABS,Airbag conducente,Airbag passeggero,Climatizzatore,Autoradio,Volante in pelle, ESP,Isofix,Specchietti laterali elettrici,Lettore CD,4 Alzacristalli elettrici,Computer di bordo,Bracciolo,Chiusura centralizzata,Portapacchi,Servosterzo,Antifurto, Immobilizzatore elettronico,Fendinebbia,Tettuccio apribile,Chiusura centralizzata telecomandata,Cerchi in lega da 16'',Ruota di riserva,Scadenza Prossima Revisione 31/10/2021,Cilindri 4,Marce 5,KW 85,Euro 3",    
+        'localita' => 'napoli',    
+        'targa' => '3b45asd',    
+        'immagine-vetrina' => 'https://i.ebayimg.com/00/s/NjAwWDgwMA==/z/fSUAAOSwXRph~BpA/$_57.JPG?set_id=880000500F',    
+    ],
+    [
+        'id' => '2',    
+        'marca' => 'pagani',    
+        'modello' => 'Huayra',    
+        'motorizzazione' => '6.0 V12 Bi-Turbo',    
+        'anno-immatricolazione' => '02/2021',    
+        'km-percorsi' => '10',    
+        'colore' => 'bronzo',    
+        'prezzo' => '2.148.000',    
+        'allestimento' => 'pazzo sgravato',    
+        'descrizione-venditore' => "
+        Pagani Huayra:
+        Esterno: Bronzo
+        Interno: Pelle marrone
+        OPZIONI:Bluetooth,Computer di bordo, Finestrini elettrici, Interfaccia MP3, Volante multifunzionale, Sistema di navigazione Servosterzo, Chiusura centralizzata, Specchietti laterali elettrici, Cerchi in lega,  Sospensioni sportive, Pacchetto sportivo, Sicurezza e ambiente, SEZIONE, ESP, Immobilizzatore elettrico,curva di luce,Sensore di luce, Fendinebbia,Sensore di pioggia,Luci di marcia diurna,Controllo della trazione,Fari allo xeno",    
+        'localita' => 'Velletri (RM)',    
+        'targa' => '3b4f5asd',    
+        'immagine-vetrina' => 'https://i.ebayimg.com/00/s/NTM0WDgwMA==/z/fzwAAOSwRb9h8OGE/$_57.JPG?set_id=880000500F',    
+    ],
+    [
+        'id' => '1',    
+        'marca' => 'mercedes benz',    
+        'modello' => 'Classe G',    
+        'motorizzazione' => '5.0 benzina 571CV',    
+        'anno-immatricolazione' => '12/2015',    
+        'km-percorsi' => '27.500',    
+        'colore' => 'grigio opaco',    
+        'prezzo' => '86.900',    
+        'allestimento' => 'amg performance',    
+        'descrizione-venditore' => "DESCRIZIONE
+            041U Grafite metallizzato G manufaktur )
+            219 (DIALOG-Code fuer interne Verwendung)
+            BB3 Programma Elettronico di Stabilità ESP
+            BS1 Pinze del freno AMG anteriori e posteriori verniciate di colore argento
+            D04 Cielo designo in microfibra DINAMICA nera
+            D27 Tetto scorrevole
+            DN1 Rete di protezione dal carico
+            EA2 Blind Spot Assist
+            EF6 Autoradio digitale
+            EH2 Telefonia Comfort
+            EH3 Kit di cavi Media Interface
+            EM8 Sintonizzatore TV
+            EU0 Guida turistica e gastronomica
+            EU2 COMAND Online
+            EU9 Sound System Surround Harman Kardon®? Logic 7®?
+            EX9 3 anni di aggiornamenti gratuiti dei dati cartografici
+            EZ8 Assistente al parcheggio PARKTRONIC
+            EZ9 Sistema di assistenza attivo alla regolazione della distanza DISTRONIC
+            F463 Serie 463 - fuoristrada
+            Visitando il nostro showroom potrete trovare le migliori auto usate di tutti i brand, sulle quali garantiamo la massima affidabilità oltre a 12 mesi di garanzia.
+            La nostra esperta venditrice vi saprà sempre indirizzare nella scelta del modello giusto, mettendo a vostra disposizione i veicoli più adatti alle vostre richieste, collaborando direttamente con le case automobilistiche e consigliando la formula di pagamento e finanziamento più vantaggiosa e conveniente.
+            Una concessionaria multimarca moderna e dinamica che sposa la competitività dei prezzi con la qualità del servizio - prima, durante e dopo la vendita.
+            Vieni a scoprire le nostre offerte o richiedi maggiori informazioni.",    
+        'localita' => 'Vertemate con Minoprio (CO)',    
+        'targa' => '46ahsda',    
+        'immagine-vetrina' => 'https://i.ebayimg.com/00/s/NDgwWDY0MA==/z/BLUAAOSwoJxh5vWv/$_57.JPG?set_id=880000500F',    
+    ],
+    [
+        'id' => '1',    
+        'marca' => 'tesla',    
+        'modello' => 'model 3',    
+        'motorizzazione' => 'eletrico 135CV',    
+        'anno-immatricolazione' => '09/2019',    
+        'km-percorsi' => '13.500',    
+        'colore' => 'bianco',    
+        'prezzo' => '38.900',    
+        'allestimento' => 'standard',    
+        'descrizione-venditore' => "[Rif. 15210589]
+            VETTURA AZIENDALE DIREZIONALE UFFICIALE, DISPONIBILE IN DIVERSI COLORI E ALLESTIMENTI,  FOTO D?ARCHIVIO , per ulteriori info allo  MOSTRA NUMERO oppure su WhatsApp MOSTRA NUMERO, www.autoardioli.itVETRI SCURI POSTERIORI
+            Specchi laterali chiudibili",    
+        'localita' => 'Guastalla (RE)',    
+        'targa' => '347dahca',    
+        'immagine-vetrina' => 'https://i.ebayimg.com/00/s/NDgwWDY0MA==/z/GncAAOSwKgNh9OF3/$_57.JPG?set_id=880000500F',    
+    ],
+];
